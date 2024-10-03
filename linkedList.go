b1f90c973e4e719b06fcc6b94cdf182d60edaeb7
kostushka <nastyatery47@yandex.ru>
2024-10-04 02:05:45 +0300

    update linkedList

--- linkedList.go
+++ linkedList.go
@@ -2,35 +2,73 @@ package main
 
 import "fmt"
 
-func main() {
-	type List struct {
+type List struct {
 		key int
 		prev *List
-	}
-	// первый элемент связанного списка
-	list := List{0, nil}
-	// адрес последнего элемента связанного списка будет лежать в переменной
-	var linked *List
-	// добавили первый элемент связанного списка в связанный список
-	linked = &list
-	
-	for i := 1; i < 10; i++ {
-		// создали новый элемент связанного списка, в нем лежит адрес предыдущего элемента
-		newList := List{i, linked}
-		// положили адрес нового элемента на место прежнего, прежний адрес остался записан в новом элементе в поле prev
-		linked = &newList
-	}
-	findElement := 47
-	isFindElement := false
+}
+
+func addElToLinked(key int, linked **List) {
+	// создали новый элемент связанного списка, в нем лежит адрес предыдущего элемента
+	newList := List{key, *linked}
+	// положили адрес нового элемента на место прежнего, прежний адрес остался записан в новом элементе в поле prev
+	*linked = &newList
+}
+
+func findElToLinked(key int, linked *List) *List {
 	// ищем элемент по связанному списку
 	for j := linked; j != nil; j = j.prev {
-		fmt.Println(j)
-		if j.key == findElement {
-			fmt.Printf("Элемент %d найден\n", j.key)
-			isFindElement = true
+		if j.key == key {
+			fmt.Printf("Элемент с ключом %d найден\n", j.key)
+			return j
 		}
 	}
-	if !isFindElement {
-		fmt.Printf("Элемент %d не найден\n", findElement)
+	fmt.Printf("Элемент %d не найден\n", key)
+	return nil
+}
+
+func printKeys(linked *List) {
+	for i := linked; i != nil; i = i.prev {
+		fmt.Println(i)
 	}
+}
+
+func deleteElToLinked(key int, linked *List) *List {
+	var prev *List
+	for i := linked; i != nil; i = i.prev {
+		if i.key == key {
+			// удаляем элемент
+			prev.prev = i.prev
+			fmt.Printf("Элемент с ключом %d был удален из связанного списка\n", i.key)
+			// возвращаем адрес удаленного элемента
+			return i
+			
+		}
+		prev = i
+	}
+	return nil
+}
+
+func main() {
+	// адрес последнего добавленного элемента связанного списка будет лежать в переменной
+	var linked *List
+	// добавляем элементы связанного списка в связанный список
+	for i := 0; i < 10; i++ {
+		addElToLinked(i, &linked)
+	}
+
+	findElement := 1
+
+	fmt.Printf("Ищем элемент с ключом %d в связанном списке:\n", findElement)
+	resFind := findElToLinked(findElement, linked)
+	fmt.Println("Адрес найденного элемента:", resFind)
+
+	fmt.Println("Вывести все ключи связанного списка:")
+	printKeys(linked)
+
+	fmt.Printf("Удаляем элемент с ключом %d из связанного списка:\n", findElement)
+	fmt.Println(deleteElToLinked(findElement, linked))
+	
+	fmt.Printf("Проверяем, что элемента с ключом %d нет в связанном списке:\n", findElement)
+	findElToLinked(findElement, linked)
+	printKeys(linked)
 }
