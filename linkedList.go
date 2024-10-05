4068a345fd7bfe95a9c44ff01b97d191698811f3
kostushka <nastyatery47@yandex.ru>
2024-10-05 03:56:22 +0300

    update

--- linkedList.go
+++ linkedList.go
@@ -26,6 +26,31 @@ func findElToLinked(key int, linked *List) *List {
 	return nil
 }
 
+func findNElToLinked(num int, linked *List) *List {
+	if num <= 0 {
+		fmt.Println("Порядковый номер искомого элемента не должен быть меньше 1")
+		return nil
+	}
+	// узнать размер связанного списка
+	linkedSize := 0
+	for i := linked; i != nil; i = i.prev {
+		linkedSize++
+	}
+	// посчитать, сколько элементов надо пропустить, чтобы дойти до искомого
+	countMissEl := linkedSize - num
+	if countMissEl < 0 {
+		fmt.Printf("Элемент с порядковым номером %d не найден. В связанном списке %d элементов\n", num, linkedSize)
+		return nil
+	}
+	// дойти до искомого элемента
+	res := linked
+	for countMissEl > 0 {
+		res = res.prev
+		countMissEl--
+	}
+	return res
+}
+
 func printElToLinked(linked *List) {
 	if linked == nil {
 		fmt.Println("Связанный список пуст")
@@ -75,20 +100,31 @@ func main() {
 
 	findElement := 1
 
+	fmt.Println("****")
 	fmt.Printf("Ищем элемент с ключом %d в связанном списке:\n", findElement)
 	resFind := findElToLinked(findElement, linked)
 	fmt.Println("Адрес найденного элемента:", resFind)
+	fmt.Println("****")
+
+	numEl := 8
+	fmt.Printf("Ищем элемент с порядковым номером %d в связанном списке:\n", numEl)
+	fmt.Printf("Адрес элемента: %v\n", findNElToLinked(numEl, linked))
+	fmt.Println("****")
 
 	fmt.Println("Вывести все ключи связанного списка:")
 	printElToLinked(linked)
+	fmt.Println("****")
 
 	fmt.Printf("Удаляем элемент с ключом %d из связанного списка:\n", findElement)
 	fmt.Println(deleteElToLinked(findElement, &linked))
+	fmt.Println("****")
 	
 	fmt.Printf("Проверяем, что элемента с ключом %d нет в связанном списке:\n", findElement)
 	findElToLinked(findElement, linked)
 	printElToLinked(linked)
+	fmt.Println("****")
 
 	fmt.Println("Последовательно удаляем элементы из связаннго списка:")
 	deleteAll(&linked)
+	fmt.Println("****")
 }
